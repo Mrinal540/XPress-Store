c4b0be4593d01e51c034c8a51c9d82dee9a1e0e9
Mrinal Anand <77539412+Mrinal540@users.noreply.github.com>
2022-04-28 00:47:28 +0530

    Final Commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+require("includes/common.php");
+
+// Redirects the user to products page if he/she is logged in.
+if (isset($_SESSION['email'])) {
+  header('location: products.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome | XPress Store</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body style="padding-top: 50px;">
+        <!-- Header -->
+        <?php
+        include 'includes/header.php';
+        ?>
+        <!--Header end-->
+
+        <div id="content">
+            <!--Main banner image-->
+            <div id = "banner_image">
+                <div class="container">	
+                    <center>
+                        <div id="banner_content">
+                            <h1>Best Way to Shop</h1>
+                            <p>Fast Delivery on Every Product You Purchase. </p>
+                            <br/>
+                            <a  href="products.php" class="btn btn-danger btn-lg active">Shop Now</a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <!--Main banner image end-->
+
+            <!--Item categories listing-->
+            <div class="container">
+                <div class="row text-center" id="item_list">
+                    <div class="col-sm-4">
+                        <a href="products.php#cameras" >
+                            <div class="thumbnail">
+                                <img src="img/13.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Electronics</h3>
+                                    <p>Our Exquisite Collection of items.</p>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#watches" >
+                            <div class="thumbnail">
+                                <img src="img/10.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Groceries</h3>
+                                    <p>Premium Quality from Best Brands.</p>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#shirts" >
+                            <div class="thumbnail">
+                                <img src="img/1.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Home-Made Items</h3>
+                                    <p>Choose Among the best available.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+            <!--Item categories listing end-->
+        </div>
+        
+        <!--Footer-->
+        <?php
+        include 'includes/footer.php';
+        ?>
+        <!--Footer end-->
+   
+    </body> 
+</html>
